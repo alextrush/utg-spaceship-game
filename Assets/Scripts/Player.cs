6550840704ec1d200c5310416df8e0f2a4e8e5ac
Alex <alextrush64@gmail.com>
2019-10-17 14:30:17 -0700

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public GameObject missile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.right * x * Time.deltaTime * speed);
+        transform.Translate(Vector2.up * y * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(missile, transform.position, Quaternion.identity);
+        }
+    }
+}
